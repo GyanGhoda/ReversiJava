14aa612e87aa39c3d7347d528b4623ef6c158dc1
GyanGhoda <gyan.ghoda@outlook.com>
2023-10-27 23:06:44 -0400

    fixed board initalization

--- src/BasicReversiModel.java
+++ src/BasicReversiModel.java
@@ -21,18 +21,23 @@ public class BasicReversiModel implements ReversiModel {
 
     private void initializeBoard(int width) {
         int middleY = (width - 1) / 2;
-        int currentRowQ = 0;
+        int currentRowStartingQ = 0;
+        int currentRowStartingS = middleY;
         int currentR = -middleY;
 
         for (int rowsMade = 0; rowsMade < width; rowsMade += 1) {
-            int currentQ = currentRowQ;
-            for (int currentS = middleY; currentS >= currentRowQ; currentS -= 1) {
+            int currentQ = currentRowStartingQ;
+
+            for (int currentS = currentRowStartingS; currentS >= currentRowStartingQ; currentS -= 1) {
                 this.board.put(new PositionAxial(currentQ, currentR, currentS), new GameCell(CellType.Empty));
                 currentQ += 1;
             }
 
             if (!(rowsMade > middleY + 1)) {
-                currentRowQ -= 1;
+                currentRowStartingQ -= 1;
+            }
+            else {
+                currentRowStartingS -= 1;
             }
             currentR += 1;
         }
