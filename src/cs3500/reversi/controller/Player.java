c1c41c236f4c9a182b12350206aca13b170972ce
GyanGhoda <gyan.ghoda@outlook.com>
2023-10-31 18:17:32 -0400

    fixed formatting issue

--- src/cs3500/reversi/controller/Player.java
+++ src/cs3500/reversi/controller/Player.java
@@ -3,8 +3,7 @@ package cs3500.reversi.controller;
 /**
  * Represents a player in a Reversi game. Not implemented yet due to controller
  * not being implemented. Generalized to allow for different types of players
- * (AI and human)
- * to be implemented into a game of Reversi.
+ * (AI and human) to be implemented into a game of Reversi.
  */
 public interface Player {
   // Represents a generic player. Not implemented yet due to controller not being
