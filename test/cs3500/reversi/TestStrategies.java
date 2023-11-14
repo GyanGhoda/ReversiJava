205f04d94cd35f76043bede7798806ad09ece788
Shreypatel2224 <87133722+Shreypatel2224@users.noreply.github.com>
2023-11-14 18:34:00 -0500

    testing strategies

--- test/cs3500/reversi/TestStrategies.java
+++ test/cs3500/reversi/TestStrategies.java
@@ -3,6 +3,8 @@ package cs3500.reversi;
 import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Test;
+import cs3500.reversi.controller.AvoidCellsNextToCorner;
+import cs3500.reversi.controller.CaptureCellsInCorner;
 import cs3500.reversi.controller.CaptureMostPieces;
 import cs3500.reversi.controller.GamePlayer;
 import cs3500.reversi.controller.Player;
@@ -52,20 +54,105 @@ public class TestStrategies {
     HashMap<PositionAxial, Cell> boardToAdd = new HashMap<PositionAxial, Cell>();
     boardToAdd.put(new PositionAxial(1, -2, 1), blackCell);
 
-    BasicReversiModelMockTranscript model = new BasicReversiModelMockTranscript(7, boardToAdd, new GamePlayer(PlayerType.WHITE));
+    BasicReversiModelMockTranscript model =
+        new BasicReversiModelMockTranscript(7, boardToAdd, new GamePlayer(PlayerType.WHITE));
 
     ReversiStrategy strat = new CaptureMostPieces();
 
     PositionAxial stratPosn = strat.chooseMove(model, PlayerType.WHITE);
 
     Assert.assertEquals(model.getLog(),
-          "getScoreForMove(Q: -2, R: 1, S: 1)\n" + 
-              "getScoreForMove(Q: 1, R: 1, S: -2)\n" + 
-              "getScoreForMove(Q: -1, R: -1, S: 2)\n" + 
-              "getScoreForMove(Q: 2, R: -1, S: -1)\n" + 
-              "getScoreForMove(Q: -1, R: 2, S: -1)\n" + 
-              "getScoreForMove(Q: 1, R: -3, S: 2)\n" + 
-              "getScoreForMove(Q: 2, R: -3, S: 1)\n");
+        "getScoreForMove(Q: -2, R: 1, S: 1)\n" + "getScoreForMove(Q: 1, R: 1, S: -2)\n"
+            + "getScoreForMove(Q: -1, R: -1, S: 2)\n" + "getScoreForMove(Q: 2, R: -1, S: -1)\n"
+            + "getScoreForMove(Q: -1, R: 2, S: -1)\n" + "getScoreForMove(Q: 1, R: -3, S: 2)\n"
+            + "getScoreForMove(Q: 2, R: -3, S: 1)\n");
     Assert.assertEquals(stratPosn, new PositionAxial(2, -3, 1));
   }
+  
+  // Test that the strategy that avoids cells next to the corner works as intended with one 
+  // move to make
+  @Test
+  public void testAvoidCellsNextToCornerOneMoveStartGame() {
+
+    Cell blackCell = new GameCell(CellType.Player);
+    Player player = new GamePlayer(PlayerType.BLACK);
+    blackCell.setCellToPlayer(player);
+
+    HashMap<PositionAxial, Cell> boardToAdd = new HashMap<PositionAxial, Cell>();
+    boardToAdd.put(new PositionAxial(1, -2, 1), blackCell);
+
+    BasicReversiModelMockTranscript model =
+        new BasicReversiModelMockTranscript(7, boardToAdd, new GamePlayer(PlayerType.WHITE));
+
+    ReversiStrategy strat = new AvoidCellsNextToCorner();
+
+    PositionAxial stratPosn = strat.chooseMove(model, PlayerType.WHITE);
+
+    Assert.assertEquals(model.getLog(),
+        "getScoreForMove(Q: -2, R: 1, S: 1)\n" + "getScoreForMove(Q: 1, R: 1, S: -2)\n"
+            + "getScoreForMove(Q: -1, R: -1, S: 2)\n" + "getScoreForMove(Q: 2, R: -1, S: -1)\n"
+            + "getScoreForMove(Q: -1, R: 2, S: -1)\n" + "getScoreForMove(Q: 1, R: -3, S: 2)\n"
+            + "getScoreForMove(Q: 2, R: -3, S: 1)\n");
+    Assert.assertEquals(stratPosn, new PositionAxial(2, -3, 1));
+  }
+
+  // Test that the strategy that captures cells in the corner works as intended with 
+  // no move to make
+  @Test
+  public void testCaptureCellsInCornerNoMoveStartGame() {
+
+    Cell blackCell = new GameCell(CellType.Player);
+    Player player = new GamePlayer(PlayerType.BLACK);
+    blackCell.setCellToPlayer(player);
+
+    HashMap<PositionAxial, Cell> boardToAdd = new HashMap<PositionAxial, Cell>();
+    boardToAdd.put(new PositionAxial(1, -2, 1), blackCell);
+
+    BasicReversiModelMockTranscript model =
+        new BasicReversiModelMockTranscript(7, boardToAdd, new GamePlayer(PlayerType.WHITE));
+
+    ReversiStrategy strat = new CaptureCellsInCorner();
+
+    PositionAxial stratPosn = strat.chooseMove(model, PlayerType.WHITE);
+
+    Assert.assertEquals(model.getLog(),
+        "getScoreForMove(Q: -2, R: 1, S: 1)\n" + "getScoreForMove(Q: 1, R: 1, S: -2)\n"
+            + "getScoreForMove(Q: -1, R: -1, S: 2)\n" + "getScoreForMove(Q: 2, R: -1, S: -1)\n"
+            + "getScoreForMove(Q: -1, R: 2, S: -1)\n" + "getScoreForMove(Q: 1, R: -3, S: 2)\n"
+            + "getScoreForMove(Q: 2, R: -3, S: 1)\n");
+    Assert.assertEquals(stratPosn, new PositionAxial(37, 37, 37));
+  }
+
+  // Test that the strategy that captures cells in the corner makes the
+  // correct choice when there is a move available in the corner
+  @Test
+  public void testCaptureCellsInCornerOneMoveStartGame() {
+
+    Cell blackCell = new GameCell(CellType.Player);
+    Player player = new GamePlayer(PlayerType.BLACK);
+    blackCell.setCellToPlayer(player);
+
+    HashMap<PositionAxial, Cell> boardToAdd = new HashMap<PositionAxial, Cell>();
+    boardToAdd.put(new PositionAxial(2, -2, 0), blackCell);
+
+    BasicReversiModelMockTranscript model =
+        new BasicReversiModelMockTranscript(7, boardToAdd, new GamePlayer(PlayerType.WHITE));
+
+    ReversiStrategy strat = new CaptureCellsInCorner();
+
+    PositionAxial stratPosn = strat.chooseMove(model, PlayerType.WHITE);
+
+    Assert.assertEquals(model.getLog(),
+        "getScoreForMove(Q: 3, R: -3, S: 0)\n" + "getScoreForMove(Q: 1, R: -2, S: 1)\n"
+            + "getScoreForMove(Q: -2, R: 1, S: 1)\n" + "getScoreForMove(Q: 1, R: 1, S: -2)\n"
+            + "getScoreForMove(Q: -1, R: -1, S: 2)\n" + "getScoreForMove(Q: 2, R: -1, S: -1)\n"
+            + "getScoreForMove(Q: -1, R: 2, S: -1)\n");
+    Assert.assertEquals(stratPosn, new PositionAxial(2, -3, 1));
+  }
+
+  
+
+
+
+
 }
