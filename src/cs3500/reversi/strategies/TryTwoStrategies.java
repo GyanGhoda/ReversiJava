277bb59db6031ab1261058eb8086fbf312462f94
Shreypatel2224 <87133722+Shreypatel2224@users.noreply.github.com>
2023-11-14 19:22:22 -0500

    testing strategies

--- src/cs3500/reversi/strategies/TryTwoStrategies.java
+++ src/cs3500/reversi/strategies/TryTwoStrategies.java
@@ -7,11 +7,11 @@ import cs3500.reversi.model.ReversiModel;
 public class TryTwoStrategies implements ReversiStrategy {
     // The two strategies that will be used. The first strategy will be used first,
     // and if it decides to pass, then the second strategy will be used.
-    ReversiStrategy firstStrategy;
-    ReversiStrategy secondStrategy;
+    private final ReversiStrategy firstStrategy;
+    private final ReversiStrategy secondStrategy;
 
     // The constructor that takes in two different ReversiStrategies
-    TryTwoStrategies(ReversiStrategy firstStrategy, ReversiStrategy secondStrategy) {
+    public TryTwoStrategies(ReversiStrategy firstStrategy, ReversiStrategy secondStrategy) {
         this.firstStrategy = firstStrategy;
         this.secondStrategy = secondStrategy;
     }
