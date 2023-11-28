fc8c3efe24da8013e01b634694ab62f437ef6cdf
GyanGhoda <gyan.ghoda@outlook.com>
2023-11-27 21:36:07 -0500

    fixed main

--- src/cs3500/reversi/Reversi.java
+++ src/cs3500/reversi/Reversi.java
@@ -1,5 +1,8 @@
 package cs3500.reversi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cs3500.reversi.controller.BasicReversiController;
 import cs3500.reversi.controller.HumanPlayer;
 import cs3500.reversi.controller.Player;
@@ -35,31 +38,14 @@ public final class Reversi {
     int argCounter = 0;
 
     try {
-      switch (args[0]) {
+      switch (args[argCounter]) {
         case "human":
+          argCounter += 1;
           player1 = new HumanPlayer(PlayerType.BLACK);
-          argCounter = 1;
           break;
-        case "strategy1":
-          player1 = new ComputerPlayer(PlayerType.BLACK, new CaptureMostPieces());
-          argCounter = 1;
-          break;
-        case "strategy2":
-          player1 = new ComputerPlayer(PlayerType.BLACK, new AvoidCellsNextToCorner());
-          argCounter = 1;
-          break;
-        case "strategy3":
-          player1 = new ComputerPlayer(PlayerType.BLACK, new CaptureCellsInCorner());
-          argCounter = 1;
-          break;
-        case "strategy4":
-          player1 = new ComputerPlayer(PlayerType.BLACK, new MinimizeNextOpponentMove());
-          argCounter = 1;
-          break;
-        case "combined":
-          player1 = new ComputerPlayer(PlayerType.BLACK,
-                  new TryTwoStrategies(getStrategy(args[1]), getStrategy(args[2])));
-          argCounter = 3;
+        case "computer":
+          argCounter = getToNextPlayer(args, argCounter + 1);
+          player1 = setUpComputer(args, argCounter, PlayerType.BLACK);
           break;
         default:
           throw new IllegalArgumentException("Player type not supported");
@@ -69,27 +55,12 @@ public final class Reversi {
         case "human":
           player2 = new HumanPlayer(PlayerType.WHITE);
           break;
-        case "strategy1":
-          player2 = new ComputerPlayer(PlayerType.WHITE, new CaptureMostPieces());
-          break;
-        case "strategy2":
-          player2 = new ComputerPlayer(PlayerType.WHITE, new AvoidCellsNextToCorner());
-          break;
-        case "strategy3":
-          player2 = new ComputerPlayer(PlayerType.WHITE, new CaptureCellsInCorner());
-          break;
-        case "strategy4":
-          player2 = new ComputerPlayer(PlayerType.WHITE, new MinimizeNextOpponentMove());
-          break;
-        case "combined":
-          player2 = new ComputerPlayer(PlayerType.WHITE,
-                  new TryTwoStrategies(getStrategy(args[argCounter + 1]),
-                          getStrategy(args[argCounter + 2])));
+        case "computer":
+          player2 = setUpComputer(args, argCounter, PlayerType.WHITE);
           break;
         default:
           throw new IllegalArgumentException("Player type not supported");
       }
-
     } catch (Exception e) {
       throw new IllegalArgumentException("Game not created: " + e.getMessage());
     }
@@ -114,19 +85,52 @@ public final class Reversi {
     model.startGame();
   }
 
-  // helper for switch statement when combining strategies
-  private static ReversiStrategy getStrategy(String strategy) {
-    switch (strategy) {
-      case "strategy1":
-        return new CaptureMostPieces();
-      case "strategy2":
-        return new AvoidCellsNextToCorner();
-      case "strategy3":
-        return new CaptureCellsInCorner();
-      case "strategy4":
-        return new MinimizeNextOpponentMove();
-      default:
-        throw new IllegalArgumentException("Strategy not supported");
+  private static int getToNextPlayer(String[] args, int argCounter) {
+    while (argCounter < args.length) {
+      if (args[argCounter].equals("human") || args[argCounter].equals("computer")) {
+        return argCounter;
+      }
     }
+    throw new IllegalArgumentException("No player 2 specified");
+  }
+
+  private static Player setUpComputer(String[] args, int argCounter, PlayerType type) {
+    List<ReversiStrategy> strats = new ArrayList<>();
+    while (argCounter < args.length) {
+      switch (args[argCounter]) {
+        case "strategy1":
+          strats.add(new CaptureMostPieces());
+          break;
+        case "strategy2":
+          strats.add(new AvoidCellsNextToCorner());
+          break;
+        case "strategy3":
+          strats.add(new CaptureCellsInCorner());
+          break;
+        case "strategy4":
+          strats.add(new MinimizeNextOpponentMove());
+          break;
+        case "human":
+        case "computer":
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid argument given");
+      }
+    }
+
+    if (strats.isEmpty()) {
+      return new ComputerPlayer(type, new CaptureMostPieces());
+    } else {
+      return new ComputerPlayer(type, composeAIStrategy(strats));
+    }
+  }
+
+  private static ReversiStrategy composeAIStrategy(List<ReversiStrategy> strats) {
+    ReversiStrategy currentStrategy = strats.get(0);
+    for (int i = 1; i < strats.size(); i += 1) {
+      currentStrategy = new TryTwoStrategies(strats.get(i), currentStrategy);
+    }
+
+    return currentStrategy;
   }
 }
