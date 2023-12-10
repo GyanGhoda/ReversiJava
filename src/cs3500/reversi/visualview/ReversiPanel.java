ccf48be548d0ec6dc828b703959c56bf4e446981
GyanGhoda <gyan.ghoda@outlook.com>
2023-12-10 14:37:22 -0500

    comments

--- src/cs3500/reversi/visualview/ReversiPanel.java
+++ src/cs3500/reversi/visualview/ReversiPanel.java
@@ -26,17 +26,49 @@ public interface ReversiPanel {
    */
   void refresh(boolean currentTurn);
 
+  /**
+   * Gets the current x of the selected position by the user.
+   *
+   * @return - Get the current x as an int of the selected position
+   */
   double getCurrentX();
 
+  /**
+   * Gets the current y of the selected position by the user.
+   *
+   * @return - Get the current y as an int of the selected position
+   */
   double getCurrentY();
 
+  /**
+   * Gets the score of the move of the selected position by the user.
+   *
+   * @return - the score of the move by the selected user
+   */
   int getScore();
 
+  /**
+   * Toggles the hints feature on and off using the 'h' key.
+   */
   void toggleHints();
 
+  /**
+   * Resizes the component in case of a window size change.
+   */
   void resizeComponent();
 
+  /**
+   * Updates the view when the mouse is clicked by the user.
+   *
+   * @param mouseX - the x coordinate of the mouse click
+   * @param mouseY - the y coordinate of the mouse click
+   */
   void mouseClickUpdateView(int mouseX, int mouseY);
 
+  /**
+   * Checks if the player has selected a position.
+   *
+   * @return - true if the user has a position selected, false otherwise.
+   */
   boolean playerSelected();
 }
